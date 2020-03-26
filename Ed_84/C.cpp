63201d2f8b4024a0ae1d518ffd78d8587a72c9be
Marella Shiva Sai Teja <shivasai962@gmail.com>
2020-03-26 18:28:53 +0530

    solved C.cpp after the contest

--- Ed_84/C.cpp
+++ Ed_84/C.cpp
@@ -1,100 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define rep(i,a,n) for (int i=a;i<n;i++)
 int tt;
+
 int main(){
-    cin>>tt;
+    tt = 1;
+    // cin>>tt;
     while(tt--){
         int n, m, k;
         cin>>n>>m>>k;
-        vector<pair<int, int> > p(k+1);
-        vector<pair<int, int> > q(k+1);
-        for(int i=1,x,y;i<=k;i++){
-            cin>>x>>y;
-            p[i].push_back({x,y});
-        }
-        for(int i=1,x,y;i<=k;i++){
-            cin>>x>>y;
-            q[i].push_back({x,y});
-        }
-        vector<pair<int, int> > s(k+1, {-1, -1});
-        int i=1;
-        for(i; i<=k; i++){
-            int a = q[i].first - p[i].first;
-            int b = q[i].second - p[i].second;
-            int l = abs(a)+abs(b);
-            if(s[l].first != -1){
-                if(s[l].first != a | s[l].second != b)
-                    break;
+        string ans = "";
+        rep(i,0,m-1) ans.push_back('L');
+	    rep(i,0,n-1) ans.push_back('U');
+	    rep(i,0,n) {
+            if (i%2==0) {
+                rep(j,0,m-1) ans.push_back('R');
+            } else {
+                rep(j,0,m-1) ans.push_back('L');
             }
-            else{
-                s[l].first = a;
-                s[l].second = b;
-            }
-        }
-        if(i<=k)
-            cout<<-1<<endl;
-
-        bool flag = true;
-        else{
-            int a=0,b=0;
-            for(int i =1;i<= k; i++){
-                if(s[i] != -1){
-                    if((s[i].first< 0 & a<0) | (s[i].first>0 & a>0) | (a==0)){
-                        if(abs(s[i].first) < abs(a)){
-                            cout<<-1<<endl;
-                            flag = false;
-                            break;
-                        }
-                        else
-                            a = s[i].first;
-                    }
-                    else{
-                        cout<<-1<<endl;
-                        flag = false;
-                        break;
-                    }
-                    if((s[i].second< 0 & b<0) | (s[i].second>0 & b>0) | (b==0)){
-                        if(abs(s[i].second) < abs(b)){
-                            cout<<-1<<endl;
-                            flag = false;
-                            break;
-                        }
-                        else
-                            b = s[i].second;
-                    }
-                    else{
-                        cout<<-1<<endl;
-                        flag = false;
-                        break;
-                    }
-
-                }
-            }
-            string ans = "";
-            int a =0,b =0;
-            if(flag == true){
-                for(int i = 1;i<=k ;i++){
-                    if(s[i].first != -1){
-                        int l = s[i].first - a;
-                        int r = s[i].second - b;
-                        for(int j = 0;j<abs(l);j++){
-                            if(l<0)
-                                ans+="L";
-                            else if(l>0)
-                                ans+="R";
-                        }
-                        for(int j = 0;j<abs(r);j++){
-                            if(r<0)
-                                ans+="D";
-                            else if(l>0)
-                                ans+="U";
-                        }
-                    }
-                }
-                cout<<ans.size()<<endl<<ans<<endl;
-            }
-        }
+		    if (i!=n-1) ans.push_back('D');
+	    }
+        cout<<ans.size()<<endl;
+        cout<<ans<<endl;
     }
     return 0;
 }
