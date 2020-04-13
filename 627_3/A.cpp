92c4c5fe44508a2925130cc28a437ecd9ac8f854
Marella Shiva Sai Teja <shivasai962@gmail.com>
2020-04-13 19:50:40 +0530

    before contest 634 division 3

--- 627_3/A.cpp
+++ 627_3/A.cpp
@@ -7,7 +7,16 @@ int tt;
 int main(){
     cin>>tt;
     while(tt--){
-        
+        int n; cin >> n;
+        vector<int> a(n);
+        bool odd = false, even = false;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            if(a[i]%2) odd = true;
+            else even = true;
+        }
+        if(odd ^ even) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
     return 0;
 }
---
+++ 634_3/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int tt;
+#define ll long long
+
+int main(){
+    while (tt--){
+        
+    }
+    return 0;
+}
